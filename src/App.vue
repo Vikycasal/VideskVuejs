8846939a8efda7c177a9f0cfa28f681c284dadc3
victoria.casalf@gmail.com <victoria.casalf@gmail.com>
2022-05-11 18:53:34 -0300

    tabla

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,123 @@
 <template>
-  <div id="app">
+  <div id="app" class="pagina">
     <NavBar />
-   
+    <h3>Home / <u>Report & Insights</u></h3>
+    <br>
+    <div class="tabla">
+    <table class="table-auto">
+      <thead>
+        <br>
+        <h2 class="titulo">Employees</h2>
+        <br>
+      </thead>
+      <div class="tabla1">
+  <thead>
+    <br>
+    <tr>
+      <th>Employee</th>
+      <th>Last Login</th>
+      <th>Department</th>
+      <th>Status</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+     <td>
+      <ul>
+        <li v-for="user in users" :key="user.id">
+        <!-- <img :src="user.profile_img" alt="imagen-user.image" /> -->
+        {{ user.first_name }}
+        {{ user.last_name }}
+        {{ user.email }}
+        </li>
+     </ul>
+     </td>
+     <td>
+      <ul>
+        <li v-for="user in users" :key="user.id">
+        {{ moment("user.sessions").fromNow() }}
+        </li>
+     </ul>
+     </td>
+          <td>
+      <ul>
+        <li v-for="user in users" :key="user.id">
+       {{ user.department }}
+        </li>
+     </ul>
+     </td>
+          <td>
+      <ul>
+        <li v-for="user in users" :key="user.id">
+      {{ user.status }}
+        </li>
+     </ul>
+     </td>
+   </tr>
+  </tbody>
+      </div>
+</table>
+<br>
+    </div>
+    <br>
+    <br>
+    <br>
   </div>
 </template>
 
 <script>
 import NavBar from "../src/components/NavBar/NavBar.vue"
 
+var moment = require("moment");
+
 export default {
   name: 'app',
   components: {
     NavBar
   },
+  data: () => ({
+    users: [],
+    moment: moment
+  }),
   async mounted() {
     // Users
     const userResponse = await fetch('/api/users');
     const users = await userResponse.json();
+
+    this.users = users.users;
     // Reports
     const reportResponse = await fetch('/api/reports');
     const reports = await reportResponse.json();
+
+    return {users}
   }
 }
 </script>
+
+<style>
+.pagina {
+  background-color: rgb(248, 248, 248);
+}
+
+.titulo {
+  margin-left: 20px;
+  font-size: 20px;
+}
+
+.tabla {
+  background-color: rgba(255, 255, 255, 0.808);
+  border-radius: 10px;
+  width: 1000px;
+  margin-left: 400px;
+  border: 2px solid rgba(236, 235, 235, 0.507);
+}
+
+.tabla1 {
+  border: 2px solid rgba(236, 235, 235, 0.507);
+  border-radius: 10px;
+  width: 950px;
+  margin-left: 20px;
+}
+
+
+</style>
