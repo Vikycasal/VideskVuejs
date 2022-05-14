efde2e0cd6244d1758a5a032d69a9e07019d0feb
victoria.casalf@gmail.com <victoria.casalf@gmail.com>
2022-05-14 09:07:30 -0300

    dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <template>
-  <div id="app" class="pagina">
-    <NavBar />
+  <div id="app" class="pagina" :class="mode">
+    <NavBar :mode="mode" @toggle="toggle"/>
     <h3 class="subtitulo">Home / <u>Report & Insights</u></h3>
-           <button class="switch" id="switch">
-           <span class="dm"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-           <path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd" />
-           </svg></span>
-           <span class="dm"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-           <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-           </svg></span>
-           </button>
+           <SwitchB />   
        <div class="pie-tabla">
        <div class="torta">
          <br>
@@ -119,6 +112,7 @@
 <script>
 import NavBar from "../src/components/NavBar/NavBar.vue"
 import GraficoT from "../src/components/Grafico/GraficoT.vue"
+import SwitchB from "../src/components/SwitchB/SwitchB.vue"
 
 
 var moment = require("moment");
@@ -127,11 +121,13 @@ export default {
   components: {
     NavBar,
     GraficoT,
+    SwitchB
     
   },
   data: () => ({
     users: [],
-    moment: moment
+    moment: moment,
+    mode: ''
   }),
   methods: {
     getLastLogin(sessions) {
@@ -143,7 +139,15 @@ export default {
       );
       return moment(getLastLogin).fromNow();
     },
+     toggle () {
+      if (this.mode === "dark") {
+        this.mode = "pagina"
+      } else {
+        this.mode = "dark"
+      }
+    }
   },
+  
   async mounted() {
     // Users
     const userResponse = await fetch('/api/users');
@@ -160,6 +164,11 @@ export default {
 <style>
 .pagina {
   background-color: rgb(248, 248, 248);
+  transition: background 0.3 ease-in-out;
+}
+
+.dark {
+  background: #192734;
 }
 
 .titulo {
@@ -281,4 +290,5 @@ export default {
 .pietexto {
   margin-left: 15px;
 }
+
 </style>
